1a77b5892c51210f889cb47654a74ce69c3a667f
Joseph Lewis III <jlewisiii@google.com>
2018-06-27 10:47:41 -0700

    fix setup script for src git directory

--- ci/scripts/setup-go.sh
+++ ci/scripts/setup-go.sh
@@ -4,7 +4,7 @@ PROGNAME=pubsubbeat
 GODIR=github.com/GoogleCloudPlatform/pubsubbeat
 
 mkdir -p $GOPATH/src/github.com/GoogleCloudPlatform
-ln -s $PWD/$PROGNAME $GOPATH/src/$GODIR
+ln -s $PWD/src $GOPATH/src/$GODIR
 
 cd $GOPATH/src/$GODIR
 echo "Gopath is: " $GOPATH
