d50e0af99e3829c09abdc8d2eeadfb6c5c54bdd9
Ben Kochie <superq@gmail.com>
2019-10-17 00:27:53 +0200

    Purge inline vendoring.
    Use go module downloads rather than try and keep vendor dir up-to-date.

--- vendor/github.com/elastic/beats/libbeat/publisher/queue/memqueue/doc.go
+++
@@ -1,4 +0,0 @@
-// Package memqueue provides an in-memory queue.Queue implementation for
-// use with the publisher pipeline.
-// The queue implementation is registered as queue type "mem".
-package memqueue
--- vendor/github.com/elastic/beats/libbeat/version/helper.go
+++
@@ -1,7 +0,0 @@
-package version
-
-// GetDefaultVersion returns the current libbeat version.
-// This method is in a separate file as the version.go file is auto generated
-func GetDefaultVersion() string {
-	return defaultBeatVersion
-}
